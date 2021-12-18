5c6999382d0592dddc782e4e48bf8e733c1ef208
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-18 10:43:46 +0100

    adding cart to add cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import SingleProduct from "./SingleProduct";
 
 const Cart = ({ cart, setcart }) => {
+  const [total, settotal] = useState();
+  useEffect(() => {
+    settotal(cart.reduce((acc, curr) => acc + Number(curr.price), 0));
+  }, []);
   return (
     <div>
       <span style={{ fontSize: 30 }}> My Cart</span>
       <br />
-      <span style={{ fontSize: 30 }}> Total : Eur.100</span>
-      <div className="productContainer">{}</div>
+      <span style={{ fontSize: 30 }}> Total : Eur.{total}</span>
+      <div className="productContainer">
+        {cart.map((prod) => (
+          <SingleProduct
+            prod={prod}
+            key={prod.id}
+            cart={cart}
+            setcart={setcart}
+          />
+        ))}
+      </div>
     </div>
   );
 };
