67ba4aefe28abd77f4083d49be17089491aa8517
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-18 09:46:51 +0100

    router version 5.2.0 installed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+/* eslint-disable no-undef */
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <BrowserRouter>
         <Header />
-      </div>
+        <div className="App">
+          <Route path="/" Component={Home} exact />
+          <Route path="/cart" Component={Cart} exact />
+        </div>
+      </BrowserRouter>
     );
   }
 }
